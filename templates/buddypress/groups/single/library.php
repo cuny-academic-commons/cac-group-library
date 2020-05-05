57f464922d41aa682e03366319f3b83bad414e2a
Boone B Gorges <boonebgorges@gmail.com>
2020-05-04 21:21:34 -0500

    Better initial appearance while load is in progress.

--- templates/buddypress/groups/single/library.php
+++ templates/buddypress/groups/single/library.php
@@ -1,3 +1,20 @@
 <div id="cac-group-library">
-	Not loaded
+	<div id="cac-group-library-inner">
+		<div class="group-library-header">
+			<h2>Library</h2>
+			<div class="header-actions">
+				<a href="/http:/commons.gc.cuny.edu/groups/wordpress-help/library/#/new" class="add-new-item-button">Add New Item</a>
+
+				<div class="library-search">
+					<input placeholder="Enter text to search library" class="library-search-input" style="background-image: url(&quot;http://commons.gc.cuny.edu/wp-content/plugins/cac-group-library//assets/img/search.svg&quot;); background-position: right 8px center; background-repeat: no-repeat; background-size: 12px; opacity: 0.75;"> <!---->
+				</div>
+			</div>
+		</div>
+
+		<div class="group-library-refreshable">
+			<div class="library-loading">
+				Loading...
+			</div>
+		</div>
+	</div>
 </div>
